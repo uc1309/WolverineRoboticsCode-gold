0d19e484fc3af3e3db71842af04af8dc5838ce48
UC1309 <arxliu@gmail.com>
2015-02-06 20:56:19 -0800

    Override Commit

--- WolverineRoboticsCode-gold/src/org/usfirst/frc/team949/robot/subsystems/DriveTrain.java
+++ WolverineRoboticsCode-gold/src/org/usfirst/frc/team949/robot/subsystems/DriveTrain.java
@@ -7,7 +7,6 @@ import static org.usfirst.frc.team949.robot.RobotMap.*;
 //import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Joystick.ButtonType;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -15,35 +14,26 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class DriveTrain extends Subsystem {
-
+	
 	RobotDrive drive;
-
-	// private Gyro gyro = RobotMap.driveGyro;
+	//private Gyro gyro = RobotMap.driveGyro;
 
 	public DriveTrain() {
 		drive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
 		drive.setInvertedMotor(MotorType.kFrontLeft, true);
 		drive.setInvertedMotor(MotorType.kRearRight, true);
 	}
-
-	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		setDefaultCommand(new JoystickDrive());
-	}
-
-	public void driveForward() {
-		drive.mecanumDrive_Polar(5, 0, 0);
-	}
-
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new JoystickDrive());
+    }
+    
+    public void driveForward() {
+    	drive.mecanumDrive_Polar(5, 0, 0);
+    }
+    
 	public void mechanumDrive(Joystick joy) {
-<<<<<<< HEAD
-		drive.mecanumDrive_Cartesian(-joy.getZ() / 3, joy.getY() / 3, joy.getX() / 3, joy.getTwist());
-	}
-
-	public void mechanumFullDrive(Joystick joy) {
-		drive.mecanumDrive_Cartesian(-joy.getZ() / 2, 0.75 * joy.getY(), 0.75 * joy.getX(), joy.getTwist());
-	}
-=======
     	drive.mecanumDrive_Cartesian(-joy.getZ() / 3, joy.getY() / 3, joy.getX() / 3, joy.getTwist());
     }
 	
@@ -55,9 +45,4 @@ public class DriveTrain extends Subsystem {
     	drive.mecanumDrive_Cartesian(0, 0, 0, 0);
     }
 }
->>>>>>> FETCH_HEAD
 
-	public void stop() {
-		drive.mecanumDrive_Cartesian(0, 0, 0, 0);
-	}
-}
